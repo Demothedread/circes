dc8a65a7c3cab0f437661543fa2a4d63744ba337
Demothedread <121599193+Demothedread@users.noreply.github.com>
2024-01-11 02:18:09 -0800

    Create scripts.js
    For Mondrian, Functions need to be filled in

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const canvas = document.getElementById('mondrianCanvas');
+const ctx = canvas.getContext('2d');
+
+function drawRandomLines() {
+    // Logic to draw random lines on the canvas
+}
+
+function generateShapes() {
+    // Logic to generate random shapes without overlapping
+}
+
+function colorShapes() {
+    // Logic to fill shapes with random colors
+}
+
+function placeButtons() {
+    // Logic to dynamically place buttons on the canvas
+}
+
+function init() {
+    drawRandomLines();
+    generateShapes();
+    colorShapes();
+    placeButtons();
+}
+
+init();
